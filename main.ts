a37a80bb0640d93b1d21cbbea90c17649bc4436a
Smart18186 <96053164+Smart18186@users.noreply.github.com>
2023-11-23 10:46:22 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,6 +12,23 @@ radio.onReceivedNumber(function (receivedNumber) {
     }
 })
 serial.redirectToUSB()
+radio.setGroup(26)
+basic.showIcon(IconNames.Yes)
+ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+ESP8266_IoT.connectWifi("Tablet", "L0r3t0kk!")
+basic.showIcon(IconNames.Heart)
+ESP8266_IoT.connectThingSpeak()
+basic.showIcon(IconNames.Cow)
+ESP8266_IoT.setData(
+"your_write_api_key",
+input.temperature()
+)
+ESP8266_IoT.uploadData()
+basic.showIcon(IconNames.Asleep)
+loops.everyInterval(500, function () {
+    basic.clearScreen()
+    serial.writeNumber(input.temperature())
+})
 basic.forever(function () {
     serial.writeNumber(input.temperature())
     serial.writeLine("")
